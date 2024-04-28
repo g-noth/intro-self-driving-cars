5d73434be007d1c7a21d86ea9ad3ece14a585d4b
swally3216 <110245151+swally3216@users.noreply.github.com>
2024-04-28 14:15:07 +0200

    create input form

--- frontend/code/main.js
+++ frontend/code/main.js
@@ -1,100 +1,110 @@
+// setup canvas
 const canvas = document.getElementById('myCanvas');
 canvas.width = 200;
-let isStopped = false;
-let animationFrame;
+const ctx = canvas.getContext('2d');
 
 const networkCanvas = document.getElementById('networkCanvas');
 networkCanvas.width = 400;
-
-// setup menu
-const laneCount = 3;
-const laneIndex = 1;
-// rayCount,raySpread,rayLength
-
-const ctx = canvas.getContext('2d');
 const networkCtx = networkCanvas.getContext('2d');
 
-const road = new Road(canvas.width / 2, canvas.width * 0.9, laneCount);
+// global variables
+let isStopped = false;
+let animationFrame;
+let road;
 
+const laneIndex = 1;
+
+initializeRoad();
 
 const N = 50;
-const cars = generateCars(N);  
-
+const cars = generateCars(N);
 let bestCar = cars[0];
-
-if(localStorage.getItem("bestBrain")){
-  for(let i=0;i<cars.length;i++){
-    cars[i].nn = JSON.parse(localStorage.getItem("bestBrain"));
-    if(i!=0){
-      NeuralNetwork.mutate(cars[i].nn,0.15); // adjust similarity to best car
+if (localStorage.getItem('bestBrain')) {
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].nn = JSON.parse(localStorage.getItem('bestBrain'));
+    if (i != 0) {
+      NeuralNetwork.mutate(cars[i].nn, 0.15); // adjust similarity to best car
     }
   }
 }
 
 const traffic = [
-  new Car(road.getLaneCenter(1), -400, 30, 50, 'TRAFFIC',2),
-  new Car(road.getLaneCenter(2), -600, 30, 50, 'TRAFFIC',2),
-  new Car(road.getLaneCenter(0), -150, 30, 50, 'TRAFFIC',2),
-  new Car(road.getLaneCenter(1), -100, 30, 50, 'TRAFFIC',2),
-  new Car(road.getLaneCenter(0), -900, 30, 50, 'TRAFFIC',2),
+  new Car(road.getLaneCenter(1), -400, 30, 50, 'TRAFFIC', 2),
+  new Car(road.getLaneCenter(2), -600, 30, 50, 'TRAFFIC', 2),
+  new Car(road.getLaneCenter(0), -150, 30, 50, 'TRAFFIC', 2),
+  new Car(road.getLaneCenter(1), -100, 30, 50, 'TRAFFIC', 2),
+  new Car(road.getLaneCenter(0), -900, 30, 50, 'TRAFFIC', 2),
 ];
 
 animate();
 
+function initializeRoad() {
+  road = new Road(canvas.width / 2, canvas.width * 0.9);
+}
+
+function setup() {
+  const lanes = document.getElementById('lanes').value;
+  initializeRoad(parseInt(lanes));
+  animate();
+}
+
 // serialize best car in local storage
-function save(){
-  localStorage.setItem("bestCarBrain",
-    JSON.stringify(bestCar.nn));
+function save() {
+  localStorage.setItem(
+    'bestCarBrain',
+    JSON.stringify(bestCar.nn),
+  );
 }
 
-function discard(){
-  localStorage.removeItem("bestCarBrain");
+function discard() {
+  localStorage.removeItem('bestCarBrain');
 }
 
-function stop(){
+function stop() {
   isStopped = !isStopped;
-  const button = document.getElementById("stopButton")
-  button.innerHTML = isStopped ? "⏯️" : "⏹️" ;
+  const button = document.getElementById('stopButton');
+  button.innerHTML = isStopped ? '⏯️' : '⏹️';
 
-  if (isStopped){
+  if (isStopped) {
     window.cancelAnimationFrame(animationFrame);
-  }else{
+  } else {
     animate();
   }
 }
 
-function reload(){
+function reload() {
   window.location.reload();
 }
 
-function generateCars(N){
-   const cars = [];
-   for(let i=1;i<N;i++){
-      cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, 'AI'));
-   }
-   return cars;
+function generateCars(N) {
+  const cars = [];
+  for (let i = 1; i < N; i++) {
+    cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, 'AI'));
+  }
+  return cars;
 }
 
 function animate(time) {
-  if(isStopped){
+  if (isStopped) {
     return;
   }
 
-  for(let i=0;i<traffic.length;i++){
+  for (let i = 0; i < traffic.length; i++) {
     traffic[i].update(road.borders, []);
   }
 
-  for(let i=0;i<cars.length;i++){
+  for (let i = 0; i < cars.length; i++) {
     cars[i].update(road.borders, traffic);
   }
   // track best car (fitness function: highest up on screen (lowest y value))
   // only works for straight roads
   bestCar = cars.find(
-    c=>c.y==Math.min(
-      ...cars.map(c=>c.y)
-    ));
+    (c) => c.y == Math.min(
+      ...cars.map((c) => c.y),
+    ),
+  );
 
-  const heightTabs = document.querySelector(".tab").clientHeight;
+  const heightTabs = document.querySelector('.tab').clientHeight;
   canvas.height = window.innerHeight;
   networkCanvas.height = window.innerHeight - heightTabs;
 
@@ -103,22 +113,22 @@ function animate(time) {
 
   road.draw(ctx);
 
-  for(let i=0;i<traffic.length;i++){
+  for (let i = 0; i < traffic.length; i++) {
     traffic[i].draw(ctx, 'red');
   }
 
   ctx.globalAlpha = 0.2;
 
-  for(let i=0;i<cars.length;i++){
+  for (let i = 0; i < cars.length; i++) {
     cars[i].draw(ctx, 'blue');
   }
-  
+
   ctx.globalAlpha = 1;
   // focus on main car with sensor
-  bestCar.draw(ctx,"blue",true);
+  bestCar.draw(ctx, 'blue', true);
 
   ctx.restore();
-  networkCtx.lineDashOffset=-time/30;
+  networkCtx.lineDashOffset = -time / 30;
   Visualizer.drawNetwork(networkCtx, bestCar.nn);
   animationFrame = requestAnimationFrame(animate);
 }
